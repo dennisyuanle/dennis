022f83e41caa3abd7a7172d23c22a1d95e265e4c
yuanle <412405659@qq.com>
2023-11-24 15:08:57 +0800

    first commit

---
+++ src/main/java/org/example/controller/FirstController.java
@@ -0,0 +1,20 @@
+package org.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class FirstController {
+
+    @RequestMapping("/first")
+    public String firstMethod(@RequestParam("name") String username, @RequestParam("pwd") String password) {
+        log.info("test log");
+        log.debug("请求参数：\n username:{},\n pwd:{}", username, password);
+        return "result : " + username + "///" + password ;
+    }
+}
